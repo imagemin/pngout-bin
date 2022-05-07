b1c044b00f30a974d71a58f4570c439afbb3b931
1000ch <shogosensui@gmail.com>
2022-05-07 23:35:56 +0900

    Update dependencies

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
-import execa from 'execa';
-import tempy from 'tempy';
+import {execa} from 'execa';
+import {temporaryDirectory} from 'tempy';
 import compareSize from 'compare-size';
 import pngout from '../index.js';
 
 test('minify a PNG', async t => {
-	const temporary = tempy.directory();
+	const temporary = temporaryDirectory();
 	const src = fileURLToPath(new URL('fixtures/test.png', import.meta.url));
 	const dest = path.join(temporary, 'test.png');
 	const args = [
